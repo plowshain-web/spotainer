bd0a8ffe1a553662088d023baeb91aeea9c155db
plowshain-web <plowshain@gmail.com>
2026-04-24 02:56:49 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Spotainer",
+  description: "PT 회원관리 프로그램",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
