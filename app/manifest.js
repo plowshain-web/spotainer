2a88fe33e6a5b2f407e2d0023a928e66e1e6a287
plowshain-web <plowshain@gmail.com>
2026-04-29 22:16:37 +0900

    Create manifest.js

---
+++ app/manifest.js
@@ -0,0 +1,33 @@
+export default function manifest() {
+  return {
+    name: "Spotainer - 여성전용 PT 회원관리",
+    short_name: "Spotainer",
+    description: "여성전용 PT 센터 현장용 회원관리 앱",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    background_color: "#111111",
+    theme_color: "#111111",
+    orientation: "any",
+    icons: [
+      {
+        src: "/icons/spotainer-icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "/icons/spotainer-icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "/icons/spotainer-maskable-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable"
+      }
+    ]
+  };
+}
