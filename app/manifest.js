6dee34ef18908152d2822b07b7f4678d388f2ed5
plowshain-web <plowshain@gmail.com>
2026-05-03 21:10:20 +0900

    Update manifest.js

--- app/manifest.js
+++ app/manifest.js
@@ -8,7 +8,7 @@ export default function manifest() {
     display: "standalone",
     background_color: "#090909",
     theme_color: "#090909",
-    orientation: "landscape",
+    orientation: "any",
     icons: [
       {
         src: "/icons/icon-192.png",
