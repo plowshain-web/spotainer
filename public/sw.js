d4bebcec942f2edd155a508002c908f5740d8a06
plowshain-web <plowshain@gmail.com>
2026-05-23 05:05:51 +0900

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // Supabase 데이터는 캐시하지 않습니다.
+});
