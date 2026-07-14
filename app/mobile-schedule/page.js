9153a61e18cbe811b3164319e102b2a978392fc8
plowshain-web <plowshain@gmail.com>
2026-07-14 15:25:41 +0900

    Update page.js

--- app/mobile-schedule/page.js
+++ app/mobile-schedule/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-export default function MobileScheduleRedirectPage() {
+export default function MobileSchedulePage() {
   useEffect(() => {
     window.location.replace("/?view=phone");
   }, []);
