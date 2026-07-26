b6f381c232b3ed39cc31910e92b7ea6e2df7d332
plowshain-web <plowshain@gmail.com>
2026-07-27 00:08:13 +0900

    Create layout.js

---
+++ app/mobile-schedule/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Spotainer 모바일",
+  description: "여성전용 PT 모바일 일정관리",
+  manifest: "/mobile-manifest.webmanifest",
+};
+
+export default function MobileScheduleLayout({ children }) {
+  return children;
+}
